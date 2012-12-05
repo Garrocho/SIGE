458f4ee9cc457986ca6882763b9c995284fdb90f
Charles Tim Batista Garrocho <ctgarrocho@gmail.com>
2012-12-05 11:38:29 -0200

    Adicionado: Borda que Esconde a Borda Anterior.

---
+++ source/src/com/sige/gui/ShadowBorder.java
@@ -0,0 +1,37 @@
+package com.sige.gui;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Insets;
+
+import javax.swing.border.AbstractBorder;
+
+public class ShadowBorder extends AbstractBorder {
+	private static final long serialVersionUID = 1L;
+	private static final int RADIUS = 5;
+
+	@Override
+	public boolean isBorderOpaque() {
+		return false;
+	}
+
+	@Override
+	public Insets getBorderInsets(Component c) {
+		return new Insets(RADIUS, RADIUS, RADIUS, RADIUS);
+	}
+
+	@Override
+	public Insets getBorderInsets(Component c, Insets insets) {
+		insets.top = RADIUS;
+		insets.left = RADIUS;
+		insets.bottom = RADIUS;
+		insets.right = RADIUS;
+		return insets;
+	}
+
+	@Override
+	public void paintBorder(Component component, Graphics g, int j, int k, int l, int i1){
+		g.setColor(Color.RED);
+	}
+}
