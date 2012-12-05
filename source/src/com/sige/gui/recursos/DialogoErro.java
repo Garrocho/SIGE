510e541eafbb578e9a0be256255a38fec2b71a5a
Charles Tim Batista Garrocho <ctgarrocho@gmail.com>
2012-12-05 11:38:52 -0200

    Adicionado: Dialogo de Mensagens Erro e Sucesso.

---
+++ source/src/com/sige/gui/recursos/DialogoErro.java
@@ -0,0 +1,76 @@
+package com.sige.gui.recursos;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Cursor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import com.sige.gui.Dialogo;
+
+public class DialogoErro extends Dialogo {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel painelNorte, painelCentro;
+	private JLabel rotuloMensagem, rotuloImagem;
+	private JButton botaoOK;
+	private String mensagem;
+	
+	public DialogoErro(Component janelaPai, String titulo, String mensagem) {
+		super();
+		this.mensagem = mensagem;
+		criarElementos();
+		customizarElementos();
+		adicionarElementos();
+		configurarEventos();
+		definirPropriedades(janelaPai, titulo, null);
+	}
+
+	@Override
+	protected void adicionarElementos() {
+		
+		painelNorte.add(rotuloImagem);
+		painelNorte.add(rotuloMensagem);
+		
+		painelCentro.add(botaoOK);
+		
+		add(painelNorte, BorderLayout.NORTH);
+		add(painelCentro, BorderLayout.CENTER);
+	}
+
+	@Override
+	protected void configurarEventos() {
+		botaoOK.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evento) {
+				getThis().dispose();
+			}
+		});
+	}
+
+	@Override
+	protected void criarElementos() {
+		painelNorte = new JPanel();
+		painelCentro = new JPanel();
+		
+		botaoOK = new JButton("OK");
+		
+		rotuloMensagem = new JLabel(this.mensagem);
+		rotuloImagem = new JLabel(new ImageIcon(getResource("cancelar.png")));
+	}
+
+	@Override
+	protected void customizarElementos() {
+		botaoOK.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		botaoOK.setToolTipText("Fechar Mensagem.");
+	}
+	
+	public DialogoErro getThis() {
+		return this;
+	}
+}
